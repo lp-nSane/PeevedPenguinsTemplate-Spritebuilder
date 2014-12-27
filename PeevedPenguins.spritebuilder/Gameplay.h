43f8ef4a493669c4ac9e5e5eac577bf29e0eda52
Lee Phillips <lee@Lees-MacBook-Pro.local>
2014-12-27 12:12:45 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Lee Phillips on 12/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
